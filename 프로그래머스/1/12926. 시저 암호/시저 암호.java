020cefa217b8f559af2eaf477d8d8d51504e7599
oHAHOo <129284199+oHAHOo@users.noreply.github.com>
2026-04-10 09:41:45 +0900

    [level 1] Title: 시저 암호, Time: 0.53 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s, int n) {
+    char[] arr = s.toCharArray();
+
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] == ' ') continue;
+
+        if (arr[i] >= 'A' && arr[i] <= 'Z') {
+            arr[i] += n;
+            if (arr[i] > 'Z') arr[i] -= 26;
+        } else if (arr[i] >= 'a' && arr[i] <= 'z') {
+            arr[i] += n;
+            if (arr[i] > 'z') arr[i] -= 26;
+        }
+    }
+
+    return new String(arr);
+}
+}
