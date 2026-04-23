ff5acfa6abda4cdec45ba5ef3bd9ee203d5d1132
oHAHOo <129284199+oHAHOo@users.noreply.github.com>
2026-04-23 09:26:25 +0900

    [level 1] Title: 명예의 전당 (1), Time: 6.92 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+            int[] answer = new int[score.length];
+            List<Integer> arr = new ArrayList<>();
+
+            for (int i = 0; i < score.length; i++) {
+                arr.add(score[i]);
+                arr.sort(Collections.reverseOrder());
+                if (arr.size() > k) {
+                    arr.remove(arr.size() - 1);
+                }
+                answer[i] = arr.get(arr.size() - 1);
+            }
+            return answer;
+        }
+}
