2d6c1fc3e18eed5243bae6d3f8a3841fa6073062
oHAHOo <129284199+oHAHOo@users.noreply.github.com>
2026-04-24 09:52:05 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int i = 0, j = 0;
+        
+        for (String word : goal) {
+            if (i < cards1.length && cards1[i].equals(word)) {
+                i++;
+            } else if (j < cards2.length && cards2[j].equals(word)) {
+                j++;
+            } else {
+                return "No";
+            }
+        }        
+        return "Yes";
+    }
+}
