45e48368a64287f5b4c9a2865fc2381d3091cb79
oHAHOo <129284199+oHAHOo@users.noreply.github.com>
2026-04-30 09:53:47 +0900

    [level 1] Title: 소수 만들기, Time: 3.03 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] nums) {
+        int count = 0;
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (isPrime(nums[i] + nums[j] + nums[k])) {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    private boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
